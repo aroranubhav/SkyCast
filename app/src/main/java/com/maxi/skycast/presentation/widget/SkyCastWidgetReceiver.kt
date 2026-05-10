e5ede97854019908c3e42c28d33f83372abfa502
Maxi <arora.anubhav93@gmail.com>
2026-05-10 21:58:30 +0530

    Add a receiver class
    This class implements Glance Broadcast receiver which receives events from the launcher.

---
+++ app/src/main/java/com/maxi/skycast/presentation/widget/SkyCastWidgetReceiver.kt
@@ -0,0 +1,12 @@
+package com.maxi.skycast.presentation.widget
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SkyCastWidgetReceiver : GlanceAppWidgetReceiver() {
+
+    override val glanceAppWidget: GlanceAppWidget
+        get() = SkyCastWidget()
+}
