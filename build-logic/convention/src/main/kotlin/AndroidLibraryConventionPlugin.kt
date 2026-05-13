efe0f02e2b992838d0079eeb4173f6f32b397c5b
Maxi <arora.anubhav93@gmail.com>
2026-05-13 14:17:46 +0530

    Add ConventionPlugin classes

---
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -0,0 +1,26 @@
+import com.android.build.gradle.LibraryExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
+
+class AndroidLibraryConventionPlugin: Plugin<Project> {
+
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.android.library")
+                apply("org.jetbrains.kotlin.android")
+            }
+            extensions.configure<LibraryExtension> {
+                compileSdk = 36
+                defaultConfig {
+                    minSdk = 26
+                }
+            }
+            extensions.configure<KotlinAndroidProjectExtension> {
+                jvmToolchain(11)
+            }
+        }
+    }
+}
