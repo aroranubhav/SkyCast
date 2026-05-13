f495b9fb1e095ce68ab2c820a07832386aba31b1
Maxi <arora.anubhav93@gmail.com>
2026-05-13 14:34:57 +0530

    Remove room plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
     alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.room)
 }
 
 val localProperties = Properties().apply {
