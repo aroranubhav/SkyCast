7966724ff440f173570f0d43529d3fe7c31d1a17
Maxi <arora.anubhav93@gmail.com>
2026-05-13 14:31:37 +0530

    Create app module for providing api key and and build config

---
+++ app/src/main/java/com/maxi/skycast/di/AppModule.kt
@@ -0,0 +1,25 @@
+package com.maxi.skycast.di
+
+import com.maxi.skycast.BuildConfig
+import com.maxi.skycast.data.di.qualifier.ApiKey
+import com.maxi.skycast.data.di.qualifier.IsDebug
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @ApiKey
+    fun provideApiKey(): String =
+        BuildConfig.API_KEY
+
+    @Provides
+    @IsDebug
+    fun provideIsDebug(): Boolean =
+        BuildConfig.DEBUG
+}
