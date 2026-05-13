79dfaf8d6d95f6a05d3d5b580344807d173c7e6a
Maxi <arora.anubhav93@gmail.com>
2026-05-13 17:09:28 +0530

    Add a class implementing WidgetUpdater
    This class calls the updateAll on the class overriding the provideGlance() method

---
+++ app/src/main/java/com/maxi/skycast/presentation/widget/SkyCastWidgetUpdater.kt
@@ -0,0 +1,13 @@
+package com.maxi.skycast.presentation.widget
+
+import android.content.Context
+import androidx.glance.appwidget.updateAll
+import com.maxi.skycast.domain.util.WidgetUpdater
+import javax.inject.Inject
+
+class SkyCastWidgetUpdater @Inject constructor(): WidgetUpdater {
+
+    override suspend fun update(context: Context) {
+        SkyCastWidget().updateAll(context)
+    }
+}
